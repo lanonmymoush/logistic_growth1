2226dcfe0658ada46f8b369c25ad964c38745bc3
Keeley Dennett <keeley.dennett@lmh.ox.ac.uk>
2023-11-20 20:18:28 +0000

    commit message

--- plot_data.R
+++ plot_data.R
@@ -1,26 +1,19 @@
 #Script to plot the logistic growth data
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment3.csv")
 
 install.packages("ggplot2")
 library(ggplot2)
+# input the data into the file, chosen data = experiment 3 
 
-ggplot(aes(t,N), data = ???) +
-  
+ggplot(aes(t,N), data = growth_data) +
   geom_point() +
-  
   xlab("t") +
-  
   ylab("y") +
-  
   theme_bw()
 
-ggplot(aes(t,???), data = growth_data) +
-  
+ggplot(aes(t,N), data = growth_data) +
   geom_point() +
-  
   xlab("t") +
-  
   ylab("y") +
-  
   scale_y_continuous(trans='log10')
--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,27 +1,30 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+library(ggplot2) 
+
+growth_data <- read.csv("experiment3.csv")
 
 logistic_fun <- function(t) {
-  
   N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
-  
   return(N)
-  
 }
 
-N0 <- ??? #
-  
-r <- ??? #
-  
-K <- ??? #
+N0 <- exp(8.512e+00) # 8.512e+00 was the number that was derived from the log transformation
+# not from the original model and so to make it back into the number from the original transformation 
+# you need to put the number into an exponential 
 
-ggplot(aes(???,???), data = growth_data) +
-  
+r <- 5.007e-03 #
+
+K <- 4.882e+09 #
+
+ggplot(aes(t,N), data = growth_data) +
   geom_function(fun=logistic_fun, colour="red") +
-  
   geom_point()
+scale_y_continuous(trans='log10')
 
-  #scale_y_continuous(trans='log10')
+sink(file = "package-versions.txt")
+sessionInfo()
+sink()
+
 
 
